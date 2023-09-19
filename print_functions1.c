6808abd018482009d0a7634f8986bdb55075ecc2
Petros Worku <139710819+petros1921@users.noreply.github.com>
2023-09-19 11:24:03 +0300

    Update print_functions1.c

--- print_functions1.c
+++ print_functions1.c
@@ -3,7 +3,7 @@
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
- * by petros and kidus
+ * By Petros and Kidus
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
